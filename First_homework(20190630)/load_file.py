6faca4669d3d64216f099089047dea2b5c6c33fc
chenguang <281255214@qq.com>
2019-07-05 14:42:18 +0800

    1

--- First_homework(20190630)/load_file.py
+++ First_homework(20190630)/load_file.py
@@ -1,29 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import re
-import pandas as pd
-
-
-def load_file(file_name, new_file, column, coding='utf-8'):
-    file = pd.read_csv(file_name, encoding=coding)
-    content = file[column].tolist()
-    for i in content:
-        try:
-            if type(i) != str:
-                continue
-            content_etl = re.findall(r'\w+', i)
-            with open(new_file, "a") as r:
-                str_content = ''.join(content_etl)
-                r.write(str_content)
-                r.write('\n')
-                r.close()
-        except:
-            print(i + '报错了，日～！')
-
-
-if __name__ == '__main__':
-    file_name_one = '../Data/movie_comments.csv'
-    file_name_two = '../Data/sqlResult_1558435.csv'
-    new_file = 'new_file.txt'
-    column = 'comment'
-    load_file(file_name_one, new_file, column)
+version https://git-lfs.github.com/spec/v1
+oid sha256:4153a5117c43f1641f04af194c8080b325fdb2fa4f95f8bcd7162d45d7d06b30
+size 844
