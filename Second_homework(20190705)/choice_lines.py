f0b3005c2ed1dce881c0f04281dea16923e98cf6
chenguang <281255214@qq.com>
2019-07-15 13:21:10 +0800

    第二次作业完成部分，ETL还有小问题，还需要调

--- Second_homework(20190705)/choice_lines.py
+++ Second_homework(20190705)/choice_lines.py
@@ -4,26 +4,70 @@ import random
 
 
 def load_line_data():
-    path_dicts = {}
     line_num = 1
+    path_dicts = {}
     with open('Data/result_station.txt', 'r', encoding='utf8') as r:
         for line in r.readlines():
             line = line.strip('\n')
             if line_num % 2 == 0:
                 station_li = line.split('->')
                 for k in range(len(station_li)):
-                    #     for path_dict in path_dicts.items():
-                    #         key , value = path_dict
-                    #         if j == key:
-                    print(11)
+                    if k == 0:
+                        append_value = station_li[k]
+                        set_value = [station_li[k + 1].strip()]
+                        for path_dict in path_dicts.items():
+                            key, value = path_dict
+                            if append_value == key:
+                                value.append(station_li[k + 1].strip())
+                                set_value = value
+                                break
+                        path_dicts[station_li[k].strip()] = set_value
+                    elif k == len(station_li) - 1:
+                        append_value = station_li[k]
+                        set_value = [station_li[k - 1].strip()]
+                        for path_dict in path_dicts.items():
+                            key, value = path_dict
+                            if append_value == key:
+                                value.append(station_li[k - 1].strip())
+                                set_value = value
+                                break
+                        path_dicts[station_li[k].strip()] = set_value
+                    else:
+                        append_value = station_li[k]
+                        set_value = [station_li[k - 1].strip(), station_li[k + 1].strip()]
+                        for path_dict in path_dicts.items():
+                            key, value = path_dict
+                            if append_value == key:
+                                value.append(station_li[k - 1].strip())
+                                value.append(station_li[k + 1].strip())
+                                set_value = value
+                                break
+                        path_dicts[station_li[k].strip()] = set_value
             line_num += 1
-    print("over")
-
-    # 广度优先
+    return path_dicts
 
 
-def breadth_first_search(start, arrive):
-    pass
+# 广度优先
+def breadth_first_search(start, arrive, connrction_grpah):
+    # path_dicts = load_line_data()
+    pathes = [[start]]
+    visitied = set()
+
+    while pathes:
+        path = pathes.pop(0)
+        froninter = path[-1]
+
+        if froninter in visitied:
+            continue
+        successors = connrction_grpah[froninter]
+
+        for city in successors:
+            new_path = path + [city]
+            pathes.append(new_path)
+
+            if city == arrive:
+                return new_path
+        visitied.add(froninter)
 
 
 # 深度优先
@@ -32,17 +76,4 @@ def depth_first_search(start, arrive):
 
 
 if __name__ == '__main__':
-    # load_line_data()
-    path_dicts = {
-        '苹果园': ['1', '2', '3']
-    }
-    station_li = ['苹果园', '古城', '八角游乐园', '八宝山', '玉泉路', '五棵松', '万寿路', '公主坟', '军事博物馆', '木樨地', '南礼士路', '复兴门', '西单', '天安门西',
-                  '天安门东', '王府井', '东单', '建国门', '永安里', '国贸', '大望路', '四惠', '四惠东']
-    for k in range(len(station_li)):
-        for path_dict in path_dicts.items():
-            key, value = path_dict
-            if station_li[k] == key:
-                value.append(station_li[k])
-            else:
-                if k == 0:
-                    path_dicts[station_li[k]] = station_li[k + 1]
+    print(breadth_first_search('西直门', '东直门', load_line_data()))
