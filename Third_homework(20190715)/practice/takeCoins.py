78c28d8ad89aa300d55f48d09d7375da0e24ef71
chenguang <281255214@qq.com>
2019-07-22 20:14:12 +0800

    第三次作业完成部分，理论问答一直答不好。求导数有点蒙圈，对谁求导数啥意思...蒙圈...

---
+++ Third_homework(20190715)/practice/takeCoins.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+
+## 我们现有1,2,5,10面值的硬币.当我们有M元时,解出最小数目的硬币组合.
+
+coin = [1, 2, 5, 10]
+
+
+def take_coins(m):
+    tc = np.array([0, 0, 0, 0])
+    tc
+    return tc
+
+
+if __name__ == '__main__':
+    List1 = [1, 2, 3, 4]
+    List2 = [5, 6, 7, 8]
+    ll = []
+    for i,j in zip(List1,List2):
+        ll.append(i*j)
+    for k in ll :
+        print(k)
